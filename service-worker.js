e4ae471bab1d21e0a2d67e871d69b3755a867bab
Romina Moya <rominamoya@gmail.com>
2018-11-28 08:04:09 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/listProducts/precache-manifest.8b97a5b0805377fd2ed039985f6c8deb.js"
+  "/products-list/precache-manifest.eb14162915a063d88f2a55d53c81cf50.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/listProducts/index.html", {
+workbox.routing.registerNavigationRoute("/products-list/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
